76c4494c814b2e0fe61793795300f2b2b604cb6b
RJ Murphy <rjmurphy777@gmail.com>
2024-02-19 15:42:59 -0500

    added in sessions

--- view.py
+++ view.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for
 from flask import request
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return '“<p>Your browser is {}</p>'.format(user_agent)
+dictionary = {
+    "homogeneous": "similar, or the same as something else.",
+    "blunt": "Having a thick edge or point, as an instrument; not sharp.",
+}
+@app.route('/', methods=['GET'])
+def post_comment():
+    if request.method == 'GET':
+        word = ""
+        data = request.args.get("Review", "").strip()
+        print(request.headers["Cookie"])
+        if data:
+            word = dictionary.get(data, "Does Not Exist")
+        session[data] = word
+        print(session)
+        return render_template('speed.html', word=session)
+    return render_template('speed.html')
+app.config['SECRET_KEY'] = 'eerenad'
+
+
