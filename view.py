02c3052bd82256e8c6a744ec27e77d94a2775864
RJ Murphy <rjmurphy777@gmail.com>
2024-03-05 09:30:25 -0500

    Got it working with a deque, still unsure if this is making the time complexity worse or not

--- view.py
+++ view.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask import request
+from collections import deque
 app = Flask(__name__)
 import redis
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
@@ -11,20 +12,22 @@ def post_comment():
             word = r.get(data)
             print(session)
             if "word" not in session:
-                session["word"] = []
+                session["word"] = deque()
+            else:
+                session["word"] = deque(session["word"])
             temp_set = set(session.get("word", []))
             if (data, word) not in temp_set:
-                session["word"].append((data, word))
+                session["word"].appendleft((data, word))
                 session.modified = True
-                session["word"].reverse()
             else:
+                session["word"] = list(session["word"])
                 index = session["word"].index((data, word))
                 new_data, new_word = session["word"].pop(index)
-                session["word"].append((new_data, new_word))
-                session["word"].reverse()
-
+                session["word"] = deque(session["word"])
+                session["word"].appendleft((new_data, new_word))
+            session["word"] = list(session["word"])
             print(session)
 
         return render_template('speed.html', words=session.get("word", []))
     return render_template('speed.html')
-app.config['SECRET_KEY'] = 'dleeoppzzzz'
+app.config['SECRET_KEY'] = 'ppepe'
