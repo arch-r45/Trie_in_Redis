1e0c8828b02ae6eef771702b5314f522c28547b4
RJ Murphy <rjmurphy777@gmail.com>
2024-03-05 09:02:58 -0500

    fixed session things

--- view.py
+++ view.py
@@ -6,15 +6,25 @@ r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 @app.route('/', methods=['GET'])
 def post_comment():
     if request.method == 'GET':
-        word = ""
         data = request.args.get("Review", "").strip().upper()
         if data:
             word = r.get(data)
+            print(session)
             if "word" not in session:
                 session["word"] = []
-            #temp_set = set(session.get("word", []))
-            session["word"].append((data, word))
+            temp_set = set(session.get("word", []))
+            if (data, word) not in temp_set:
+                session["word"].append((data, word))
+                session.modified = True
+                session["word"].reverse()
+            else:
+                index = session["word"].index((data, word))
+                new_data, new_word = session["word"].pop(index)
+                session["word"].append((new_data, new_word))
+                session["word"].reverse()
 
-        return render_template('speed.html', words=session.get('word', []))
+            print(session)
+
+        return render_template('speed.html', words=session.get("word", []))
     return render_template('speed.html')
-app.config['SECRET_KEY'] = 'deddldlddll'
+app.config['SECRET_KEY'] = 'dleeoppzzzz'
