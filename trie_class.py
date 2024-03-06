ea290f671d4d279e34c32397e4d092fec15d2ebe
RJ Murphy <rjmurphy777@gmail.com>
2024-03-05 21:04:34 -0500

    added Trie Class with autocomplete methods

---
+++ trie_class.py
@@ -0,0 +1,39 @@
+import copy
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+    def autocomplete(self, prefix):
+        res = []
+        node = self.root
+        curr = ""
+        for char in prefix:
+            if char not in node.children:
+                return None
+            curr += char
+            node = node.children[char]
+        if node.word == True:
+            res.append(curr)
+        def dfs(cur, node):
+            if node.word == True:
+                res.append("".join(cur))
+            if node == None:
+                return
+            
+            for children in node.children:
+                cur.append(children)
+                dfs(cur, node.children[children])
+                cur.pop()
+        cur = list(curr)
+        dfs(cur, node)
+        return res
